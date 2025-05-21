14455ab68143dcf51370c4098746ae3a9fbae52c
jim <12212413@mail.sustech.edu.cn>
2025-05-21 18:23:56 +0800

    信息脱敏

--- src/main/java/com/yupi/yuaiagent/demo/invoke/OllamaAiInvoke.java
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/OllamaAiInvoke.java
@@ -5,12 +5,13 @@ import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 /**
  * Spring AI 框架调用 AI 大模型（Ollama）
  */
 // 取消注释后，项目启动时会执行
-//@Component
+@Component
 public class OllamaAiInvoke implements CommandLineRunner {
 
     @Resource
--- src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
+++ src/main/java/com/yupi/yuaiagent/demo/invoke/TestApiKey.java
@@ -6,5 +6,5 @@ package com.yupi.yuaiagent.demo.invoke;
 public interface TestApiKey {
 
     // 修改为你的 API Key
-    String API_KEY = "sk-d9288c8af62443e98bebb7484b8a4302";
+    String API_KEY = "";
 }
